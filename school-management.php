9369de1e1551361b5998a83ae238492911bca041
ARIFUR RAHMAN KHAN <45615936+Arifur97@users.noreply.github.com>
2022-10-11 06:47:45 +0600

    School Management
    School Management

---
+++ school-management.php
@@ -0,0 +1,42 @@
+<?php
+
+defined( 'ABSPATH' ) || die();
+
+if ( ! defined( 'WLSM_PLUGIN_URL' ) ) {
+	define( 'WLSM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+if ( ! defined( 'WLSM_PLUGIN_DIR_PATH' ) ) {
+	define( 'WLSM_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+final class WLSM_School_Management {
+	private static $instance = NULL;
+
+	private function __construct() {
+		$this->initialize_hooks();
+		$this->setup_database();
+	}
+
+	public static function get_instance() {		
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	private function initialize_hooks() {
+		if ( is_admin() ) {
+			require_once WLSM_PLUGIN_DIR_PATH . 'admin/admin.php';
+		}
+		require_once WLSM_PLUGIN_DIR_PATH . 'public/public.php';
+	}
+
+	private function setup_database() {
+		require_once WLSM_PLUGIN_DIR_PATH . 'admin/inc/WLSM_Database.php';
+		register_activation_hook( __FILE__, array( 'WLSM_Database', 'activation' ) );
+		register_deactivation_hook( __FILE__, array( 'WLSM_Database', 'deactivation' ) );
+		register_uninstall_hook( __FILE__, array( 'WLSM_Database', 'uninstall' ) );
+	}
+}
+WLSM_School_Management::get_instance();
